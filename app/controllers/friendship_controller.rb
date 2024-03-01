2c7c3bfb1564796ec20974dc95dffb4721bfae4d
megheshsolanki <megheshsolanki@gmail.com>
2024-03-01 16:47:37 +0530

    friend request cooldown added

--- app/controllers/friendship_controller.rb
+++ app/controllers/friendship_controller.rb
@@ -4,14 +4,40 @@ class FriendshipController < ApplicationController
     def create 
         @user = @current_user 
         @friend = User.find(params[:id])
-        @friendship = Friendship.new(sender_id: @current_user.id, reciever_id: @friend.id, status:'pending')
-        @notification = Notification.new(sender_id: @current_user.id, reciever_id: @friend.id,notification_type: 'friendship request')
-        if @friendship.save 
-            if @notification.save 
-                render json: {note: "Notification sent", sender: @user.name, reciever: @friend.name, message: "Friend request sent"}, status: :ok
-            else 
-                render json: {sender: @user.name, reciever: @friend.name, message: "Friend request not send", error: @friendship.errors.full_messages}, status: :unprocessable_entity
-            end
+
+        @check_friend = Friendship.find_by(sender_id: @user,reciever_id: @friend.id)
+        if @check_friend
+          case @check_friend.status
+          when "accepted" 
+            render json: {message: "Already friends"}, status: :ok 
+          when "pending"
+            render json: {message: "request neither accepted nor denied"}, status: :ok  
+          when "declined"
+            if @check_friend.created_at <= 30.days.ago
+              @check_friend.destroy
+              @friendship = Friendship.new(sender_id: @current_user.id, reciever_id: @friend.id, status:'pending')
+              @notification = Notification.new(sender_id: @current_user.id, reciever_id: @friend.id,notification_type: 'friendship request')
+              if @friendship.save 
+                  if @notification.save 
+                      render json: {note: "Notification sent", sender: @user.name, reciever: @friend.name, message: "Friend request sent"}, status: :ok
+                  else 
+                      render json: {sender: @user.name, reciever: @friend.name, message: "Friend request not send", error: @friendship.errors.full_messages}, status: :unprocessable_entity
+                  end
+              end
+            else
+              render json: {message: "request declined (on cooldown)"}, status: :ok
+            end      
+          end
+        else
+          @friendship = Friendship.new(sender_id: @current_user.id, reciever_id: @friend.id, status:'pending')
+          @notification = Notification.new(sender_id: @current_user.id, reciever_id: @friend.id,notification_type: 'friendship request')
+          if @friendship.save 
+              if @notification.save 
+                  render json: {note: "Notification sent", sender: @user.name, reciever: @friend.name, message: "Friend request sent"}, status: :ok
+              else 
+                  render json: {sender: @user.name, reciever: @friend.name, message: "Friend request not send", error: @friendship.errors.full_messages}, status: :unprocessable_entity
+              end
+          end
         end
     end
 
@@ -36,11 +62,8 @@ class FriendshipController < ApplicationController
         @friendship = Friendship.find_by(sender_id: @sender.id, reciever_id: @user.id)
         @notifications = Notification.find_by(sender_id: @sender.id,reciever_id: @user.id, notification_type: 'friendship request')
         @notification.destroy
-        if @friendship.destroy
-          render json: {message: "Friend request declined"}, status: :ok
-        else 
-          render json: {message: "Friend request not declined"},status: :unprocessable_entity
-        end
+        @friendship.status = "declined"
+        render json: {message: "Friend request declined"}, status: :ok
       end
 
 end
