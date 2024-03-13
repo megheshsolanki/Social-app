3c96191444b59b17fbb5940a84f63e506c53a32f
megheshsolanki <megheshsolanki@gmail.com>
2024-03-13 17:57:19 +0530

    more specs added for user controller

--- spec/requests/user_controller_spec.rb
+++ spec/requests/user_controller_spec.rb
@@ -55,6 +55,40 @@ RSpec.describe "UserControllers", type: :request do
       end
     end
   end
+  describe "get /" do
+    context "valid parameters" do
+      let(:user) {FactoryBot.create(:user)}
+      let(:access_token) { jwt_encode(user_id: user.id) }
+        before do 
+          post "/login", params: {
+            email: user.email,
+            password: user.password
+          }
+          access_token = json['access_token']
+          get "/", headers: { "Authorization" => "Bearer #{access_token}" }
+        end
+        it 'returns an array of users' do
+          expect(json).to be_an_instance_of(Array)
+        end
+        it 'returns all the users' do
+            expect(json.length).to eq(User.count-1)
+        end
+        it "returns ok status code" do
+          expect(response).to have_http_status(:ok)
+        end
+    end
+    context "invalid header" do
+        before do 
+          get "/", headers: { "Authorization" => "someInvalidToken" }
+        end
+        it "returns error" do
+          expect(json['error']).to be_present 
+        end
+        it "returns 401 status code" do
+          expect(response).to have_http_status(:unauthorized)
+        end
+    end
+  end
   describe "get /show" do
     context "valid headers" do
       let(:user) {FactoryBot.create(:user)}
@@ -79,7 +113,7 @@ RSpec.describe "UserControllers", type: :request do
           get "/show", headers: { "Authorization" => "someInvalidToken" }
         end
         it "returns error" do
-          expect(json['error']).to be_present 
+          expect(json['error']).to be_present
         end
         it "returns 401 status code" do
           expect(response).to have_http_status(:unauthorized)
@@ -95,6 +129,50 @@ RSpec.describe "UserControllers", type: :request do
         it "returns 401 status code" do
           expect(response).to have_http_status(:unauthorized)
         end
+    end
+
+    describe "patch /update" do
+      context "valid params" do
+        let(:user) {FactoryBot.create(:user)}
+        let(:access_token) { jwt_encode(user_id: user.id) }
+        let(:updated_name) {Faker::Name.name}
+          before do 
+            post "/login", params: {
+              email: user.email,
+              password: user.password
+            }
+            access_token = json['access_token']
+            patch "/update",params: {name: updated_name}, headers: { "Authorization" => "Bearer #{access_token}" }
+          end
+          it "returns the correct updated user" do
+            expect(json['email']).to eql(user.email)
+          end
+          it "returns the updated user's name" do
+            expect(json['name']).to eql(updated_name)
+          end
+          it "returns ok status code" do
+            expect(response).to have_http_status(:ok)
+          end
+      end
+      context "invalid params" do
+        let(:user) {FactoryBot.create(:user)}
+        let(:access_token) { jwt_encode(user_id: user.id) }
+        let(:phone_number) {"1234567"}
+          before do 
+            post "/login", params: {
+              email: user.email,
+              password: user.password
+            }
+            access_token = json['access_token']
+            patch "/update",params: {phone_number: phone_number}, headers: { "Authorization" => "Bearer #{access_token}" }
+          end
+          it "returns the errors array" do
+            expect(json['errors']).to be_present
+          end
+          it "returns unproccessable content status code" do
+            expect(response).to have_http_status(:unprocessable_entity)
+          end
+      end
     end
   end
 end
