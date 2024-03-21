74b6f8ef89dec61b416fa1e6db620bf2ddec7ec4
megheshsolanki <megheshsolanki@gmail.com>
2024-03-21 15:34:37 +0530

    changes for status code

--- app/controllers/forgot_password_controller.rb
+++ app/controllers/forgot_password_controller.rb
@@ -6,12 +6,10 @@ class ForgotPasswordController < ApplicationController
             @user.generate_otp
             if @user.save
               ForgotPasswordMailer.with(user_id: @user.id).forgot_password.deliver_now
-              render json: {message: "OTP sent" }
-            else
-              render json: {errors: @user.errors.full_messages}, status: :unprocessable_entity
+              render json: {message: "OTP sent" }, status: :ok
             end
         else
-            render json: { error: "Email address not found." }, status: :not_found
+            render json: { error: "User not found." }, status: :not_found
         end
     end
 
@@ -19,7 +17,7 @@ class ForgotPasswordController < ApplicationController
         @user = User.find_by(otp: params[:otp])
         if @user && @user.otp_sent_at >= 2.hours.ago
           if @user.update(password: params[:password])
-            render json: { message: "Password reset successful." }
+            render json: { message: "Password reset successful." }, status: :ok
           else
             render json: { error: @user.errors.full_messages }, status: :unprocessable_entity
           end
@@ -27,5 +25,5 @@ class ForgotPasswordController < ApplicationController
           render json: { error: "Invalid or expired OTP." }, status: :unprocessable_entity
         end
     end
-      
+
 end
